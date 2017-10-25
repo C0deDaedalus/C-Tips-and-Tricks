41bfd273d7d16fdec935ae9f28f1d64e73f3d446
Amit Kumar <31804724+C0deDaedalus@users.noreply.github.com>
2017-10-25 23:25:04 +0530

    Create hello-world-without-semicolon.c
    Printing Hello World without Using semicolon in printf(...); function

---
+++ hello-world-without-semicolon.c
@@ -0,0 +1,76 @@
+
+// Printing Hello World without using semi-colon in printf("...",...); function
+// -----------------------------------------------------------------------------
+// Why would you want to ? If your keyboard is missing semi-colon, you have deeper problems.
+// If you wish to write Obfuscated Code in C, you gotta get it !
+
+#include<stdio.h>
+
+#define HELLO printf("in #define, Hello World\n"+12) 
+
+void hello(int a, int b, int c)
+{
+}
+
+void hellochar(char k)
+{
+}
+
+int main(void *print[printf("Hello World in function passed as argument to main\n")])
+{   
+    int z=printf("HelloWorld zzzzz. . . . \n");
+    {
+    }
+    
+    char y=printf("HelloWorld yyyy. . . . \n");
+    {
+    }
+    
+    hello(printf("hello\n"),printf(" world "),printf("inside a function"));
+    {
+    }
+    
+    hello(printf("inside a function\n"),printf(" world "),printf("hello"));
+    {
+    }
+    
+    hellochar(printf("Hello World\n"));
+    {
+    }
+    
+    int x;
+    x=1?printf("Hello World in Conditional OPerator\n"):0;
+    {
+    }
+    
+    if(!printf("Hello World inside If\n"))
+    {
+    }
+
+    if(printf("Hello World inside If\n"))
+    {
+    }
+    
+    if(!printf("Hello ")) {}
+    else if(printf("World in Else\n"))
+    {
+    }
+    
+    if(HELLO)
+    {
+    }
+    
+    while(!printf("Hello world in While!\n"))
+    {
+    }
+    
+    for(;!printf("Hello world in FOR Loop\n");)
+    {
+    }
+
+    switch(printf("Hello World inside switch\n"))
+    {
+    }
+    
+    return(0);
+}
